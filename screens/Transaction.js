060bf342f42dcf5a401ac3e487e3f0e6a5652cbf
WHJR_PRO-1:4 <72431927+rashmi-sathya@users.noreply.github.com>
2022-01-20 18:36:16 +0530

    Update Transaction.js

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
+
 import {
   View,
   StyleSheet,
   TextInput,
   TouchableOpacity,
   Text,
-  ImageBackground,
-  Image
+ 
 } from "react-native";
 import * as Permissions from "expo-permissions";
 import { BarCodeScanner } from "expo-barcode-scanner";
 
-const bgImage = require("../assets/background2.png");
-const appIcon = require("../assets/appIcon.png");
-const appName = require("../assets/appName.png");
+
 
 export default class TransactionScreen extends Component {
   constructor(props) {
@@ -26,7 +24,6 @@ export default class TransactionScreen extends Component {
       scanned: false
     };
   }
-
   getCameraPermissions = async domState => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
 
@@ -57,9 +54,8 @@ export default class TransactionScreen extends Component {
       });
     }
   };
-
   render() {
-    const { bookId, studentId, domState, scanned } = this.state;
+     const { bookId, studentId, domState, scanned } = this.state;
     if (domState !== "normal") {
       return (
         <BarCodeScanner
@@ -68,13 +64,10 @@ export default class TransactionScreen extends Component {
         />
       );
     }
+    
     return (
       <View style={styles.container}>
-        <ImageBackground source={bgImage} style={styles.bgImage}>
-          <View style={styles.upperContainer}>
-            <Image source={appIcon} style={styles.appIcon} />
-            <Image source={appName} style={styles.appName} />
-          </View>
+        
           <View style={styles.lowerContainer}>
             <View style={styles.textinputContainer}>
               <TextInput
@@ -105,73 +98,52 @@ export default class TransactionScreen extends Component {
               </TouchableOpacity>
             </View>
           </View>
-        </ImageBackground>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFFFFF"
-  },
-  bgImage: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center"
-  },
-  upperContainer: {
-    flex: 0.5,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  appIcon: {
-    width: 200,
-    height: 200,
-    resizeMode: "contain",
-    marginTop: 80
-  },
-  appName: {
-    width: 80,
-    height: 80,
-    resizeMode: "contain"
-  },
-  lowerContainer: {
-    flex: 0.5,
-    alignItems: "center"
-  },
-  textinputContainer: {
-    borderWidth: 2,
-    borderRadius: 10,
-    flexDirection: "row",
-    backgroundColor: "#9DFD24",
-    borderColor: "#FFFFFF"
-  },
-  textinput: {
-    width: "57%",
-    height: 50,
-    padding: 10,
-    borderColor: "#FFFFFF",
-    borderRadius: 10,
-    borderWidth: 3,
-    fontSize: 18,
-    backgroundColor: "#5653D4",
-    fontFamily: "Rajdhani_600SemiBold",
-    color: "#FFFFFF"
-  },
-  scanbutton: {
-    width: 100,
-    height: 50,
-    backgroundColor: "#9DFD24",
-    borderTopRightRadius: 10,
-    borderBottomRightRadius: 10,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  scanbuttonText: {
-    fontSize: 24,
-    color: "#0A0101",
-    fontFamily: "Rajdhani_600SemiBold"
-  }
+
+  const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: "#FFFFFF"
+ },
+ lowerContainer: {
+   flex: 0.5,
+   alignItems: "center"
+ },
+ textinputContainer: {
+   borderWidth: 2,
+   borderRadius: 10,
+   flexDirection: "row",
+   backgroundColor: "#9DFD24",
+   borderColor: "#FFFFFF"
+ },
+ textinput: {
+   width: "57%",
+   height: 50,
+   padding: 10,
+   borderColor: "#FFFFFF",
+   borderRadius: 10,
+   borderWidth: 3,
+   fontSize: 18,
+   backgroundColor: "#5653D4",
+   color: "#FFFFFF"
+ },
+ scanbutton: {
+   width: 100,
+   height: 50,
+   backgroundColor: "#9DFD24",
+   borderTopRightRadius: 10,
+   borderBottomRightRadius: 10,
+   justifyContent: "center",
+   alignItems: "center"
+ },
+ scanbuttonText: {
+   fontSize: 24,
+   color: "#0A0101",
+ }
+
+  
 });
